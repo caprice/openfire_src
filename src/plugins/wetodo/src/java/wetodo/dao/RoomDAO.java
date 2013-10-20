fcb2377d3d740a1654589b3811994482ce66d6d1
jiangrongyong <jiangrongyong@gmail.com>
2013-10-20 15:46:29 +0800

    del no use method

--- src/plugins/wetodo/src/java/wetodo/dao/RoomDAO.java
+++ src/plugins/wetodo/src/java/wetodo/dao/RoomDAO.java
@@ -10,7 +10,6 @@ import java.util.List;
 
 public class RoomDAO {
     private static final String LIST_ROOM = "select t1.roomID,t1.roomJID,t1.jid userjid,t1.subject,t1.description,t1.creationDate,t2.jid ownerjid from (select r.roomID,r.roomJID,m.jid,r.subject,r.description,r.creationDate from ofMucRoom r,(select jid,roomID from ofMucMember union ALL SELECT jid,roomid from ofMucAffiliation) m where r.roomId = m.roomid and m.jid = ?)t1,ofMucAffiliation t2 where t1.roomID = t2.roomID and t2.affiliation = 10";
-    private static final String FIND_BY_ROOMJID = "select roomID,roomJID,subject,description,creationDate from ofMucRoom where roomJID = ?";
     private static final String FIND_BY_ROOMJID_DETAIL = "select t1.roomID,t1.roomJID,t1.subject,t1.description,t1.creationDate,t2.jid ownerjid from (select roomID,roomJID,subject,description,creationDate from ofMucRoom where roomJID = ?) t1,ofMucAffiliation t2 where t1.roomID = t2.roomID and t2.affiliation = 10";
     private static final String FETCH_MEMBER_LIST = "select userjid,name from (select userjid from (select roomID,roomJID from ofMucRoom)t1,(select jid userjid,roomID from ofMucMember union ALL SELECT jid,roomid from ofMucAffiliation)t2 where t1.roomID = t2.roomID and roomJID = ?)t3 left join ofUser t4 on t3.userjid = concat(t4.username,'@mbp.local')";
 
@@ -80,32 +79,6 @@ public class RoomDAO {
             return null;
         } catch (Exception e) {
             e.printStackTrace();
-            return null;
-        } finally {
-            DbConnectionManager.closeConnection(pstmt, con);
-        }
-    }
-
-    public static Room findByRoomJid(String roomJid) {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        try {
-            con = DbConnectionManager.getConnection();
-            pstmt = con.prepareStatement(FIND_BY_ROOMJID);
-            pstmt.setString(1, roomJid);
-            System.out.println(pstmt);
-            ResultSet rs = pstmt.executeQuery();
-            while (rs.next()) {
-                Room room = new Room();
-                room.setRoomid(rs.getInt(1));
-                room.setRoomJid(rs.getString(2));
-                room.setSubject(rs.getString(3));
-                room.setDescription(rs.getString(4));
-                room.setCreationdate(new Timestamp(Long.parseLong(rs.getString(5))));
-                return room;
-            }
-            return null;
-        } catch (SQLException sqle) {
             return null;
         } finally {
             DbConnectionManager.closeConnection(pstmt, con);
